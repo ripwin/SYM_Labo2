a574a3a5181ac303bd8a8226d613e185fa3c744c
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2017-10-27 10:14:51 +0200

    Add Person and Phone

--- app/src/main/java/com/heigvd/sym/labo2/ObjectTransmissionActivity.java
+++ app/src/main/java/com/heigvd/sym/labo2/ObjectTransmissionActivity.java
@@ -2,12 +2,38 @@ package com.heigvd.sym.labo2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ObjectTransmissionActivity extends AppCompatActivity {
+
+    private Button jsonButton;
+    private Button xmlButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_object_transmission);
+
+        this.jsonButton = (Button) findViewById(R.id.jsonButton);
+        this.xmlButton = (Button) findViewById(R.id.xmlButton);
+
+
+        // Json button action
+        this.jsonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        // Xml button action
+        this.xmlButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
