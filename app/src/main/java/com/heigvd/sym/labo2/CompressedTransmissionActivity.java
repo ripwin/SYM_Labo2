58f1732679d2bfd92b078fc8ad55af23e8862f52
Michela Zucca <michela.zucca@heig-vd.ch>
2017-11-10 08:51:35 +0100

    modif

--- app/src/main/java/com/heigvd/sym/labo2/CompressedTransmissionActivity.java
+++ app/src/main/java/com/heigvd/sym/labo2/CompressedTransmissionActivity.java
@@ -2,14 +2,44 @@ package com.heigvd.sym.labo2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class CompressedTransmissionActivity extends AppCompatActivity {
 
     private AsyncTransmission asyncTransmission = new AsyncTransmission();
+    private RadioButton rbText;
+    private RadioButton rbJson;
+    private RadioButton rbXml;
+
+    private Button bSend;
+    private TextView textResponse;
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compressed_transmission);
+        this.rbText = (RadioButton) findViewById(R.id.Text);
+        this.rbJson = (RadioButton) findViewById(R.id.Json);
+        this.rbXml = (RadioButton) findViewById(R.id.XML);
+        this.textResponse = (TextView) findViewById(R.id.textResponse);
+        this.bSend = (Button) findViewById(R.id.sendButton);
+        this.url = "http://sym.iict.ch/";
+
+        bSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(rbText.isSelected()){
+
+                }else if(rbJson.isSelected()){
+
+                }else{
+
+                }
+            }
+        });
     }
 }
